9d7694446e082e677da37b600dc8cb0e01eb8557
KyouShou <j_123321@hotmail.com>
2023-03-13 22:48:52 +0800

    重構測試

--- TDD.MagnetoEffect.Tests/MagnetoEffectTest.cs
+++ TDD.MagnetoEffect.Tests/MagnetoEffectTest.cs
@@ -4,12 +4,15 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace TDD.MagnetoEffect.Tests
 {
     internal class MagnetoEffectTest
     {
         MagnetoEffect _magnetoEffect;
+        Point _beforePoint;
+        Point _afterPoint;
 
         [SetUp]
         public void Setup()
@@ -25,24 +28,44 @@ namespace TDD.MagnetoEffect.Tests
 
         [Test]
         public void No_Anchor()
-        {          
-            Point before = new Point(49, 50);
+        {
+            Given_Mouse_Was_At(49, 50);
 
-            Point after = _magnetoEffect.Check(before);
+            When_Check();
 
-            Assert.AreEqual(before, after);
+            Assert.AreEqual(_beforePoint, _afterPoint);
         }
 
         [Test]
         public void One_Near_Anchor()
         {
-            _magnetoEffect.AddAnchor(new Point(50, 50));
+            Given_Anchor_Is(50, 50);
 
-            Point before = new Point(49, 50);
+            Given_Mouse_Was_At(49, 50);
 
-            Point after = _magnetoEffect.Check(before);
+            When_Check();
 
-            Assert.AreEqual(new Point(50, 50), after);
+            Then_Mouse_New_Position_Is(50, 50);
+        }
+
+        private void Given_Mouse_Was_At(int x, int y)
+        {
+            _beforePoint = new Point(x, y);
+        }
+
+        private void Given_Anchor_Is(int x, int y)
+        {
+            _magnetoEffect.AddAnchor(new Point(x, y));
+        }
+
+        private void When_Check()
+        {
+            _afterPoint = _magnetoEffect.Check(_beforePoint);
+        }
+
+        private void Then_Mouse_New_Position_Is(int x, int y)
+        {
+            Assert.AreEqual(new Point(x, y), _afterPoint);
         }
     }
 }
