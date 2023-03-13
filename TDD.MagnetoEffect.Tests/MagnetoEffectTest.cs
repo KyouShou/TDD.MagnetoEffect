9077ee342c1dbd6736a209883f52636d09cb1912
KyouShou <j_123321@hotmail.com>
2023-03-13 18:50:29 +0800

    TDD.執行測試-無錨點 fail

--- TDD.MagnetoEffect.Tests/MagnetoEffectTest.cs
+++ TDD.MagnetoEffect.Tests/MagnetoEffectTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,22 @@ namespace TDD.MagnetoEffect.Tests
 {
     internal class MagnetoEffectTest
     {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void No_Anchor()
+        {
+            MagnetoEffect magnetoEffect = new MagnetoEffect();
+
+            Point before = new Point(49, 50);
+
+            Point after = magnetoEffect.Check(before);
+
+            Assert.AreEqual(before , after);
+        }
+
     }
 }
---
+++ TDD.MagnetoEffect/MagnetoEffect.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.MagnetoEffect
+{
+    public class MagnetoEffect
+    {
+        //public MagnetoEffect(Point arch)
+        //{
+        
+        //}
+
+        public Point Check(Point point)
+        {
+            return new Point();
+        }
+
+    }
+}
